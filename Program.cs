fa951b58f4fa123c3b44429bea86497d87fce191
maikebing <mysticboy@live.com>
2026-02-27 16:26:27 +0800

    修改无法找到WinDivert的问题

--- Program.cs
+++ Program.cs
@@ -22,29 +22,41 @@ internal static class Program
         NativeLibrary.SetDllImportResolver(typeof(Program).Assembly, MyDllImportResolver);
         Application.Run(new MainForm());
 	}
+    static nint windivhandle= nint.Zero;
     private static IntPtr MyDllImportResolver(string libraryName, Assembly assembly, DllImportSearchPath? searchPath)
     {
         var appdir = Path.Combine(
              Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
              "TrafficPilot");
+        if (!Directory.Exists(appdir))
+        {
+            Directory.CreateDirectory(appdir);
+        }
         // 尝试从自定义路径加载
         if (libraryName == "WinDivert.dll")
         {
-            var windiv = new FileInfo(Path.Combine(appdir, "WinDivert.dll"));
-            var winsys = new FileInfo(Path.Combine(appdir, "WinDivert64.sys"));
-            if (!windiv.Exists)
+            if (windivhandle == nint.Zero)
             {
-                System.IO.File.WriteAllBytes(windiv.FullName, Properties.Resources.WinDivert);
+                var windiv = new FileInfo(Path.Combine(appdir, "WinDivert.dll"));
+                var winsys = new FileInfo(Path.Combine(appdir, "WinDivert64.sys"));
+                if (!windiv.Exists)
+                {
+                    System.IO.File.WriteAllBytes(windiv.FullName, Properties.Resources.WinDivert);
+                }
+                if (!winsys.Exists)
+                {
+                    System.IO.File.WriteAllBytes(winsys.FullName, Properties.Resources.WinDivertSys);
+                }
+                if (NativeLibrary.TryLoad(windiv.FullName, out nint _windivhandle))
+                {
+                    windivhandle = _windivhandle;
+                }
             }
-            if (!winsys.Exists)
-            {
-                System.IO.File.WriteAllBytes(winsys.FullName, Properties.Resources.WinDivertSys);
-            }
-            if (NativeLibrary.TryLoad(windiv.FullName, out nint windivhandle))
+            else
             {
                 return windivhandle;
             }
-        }
+            }
         // 返回 IntPtr.Zero 表示让运行时继续使用默认搜索逻辑
         return IntPtr.Zero;
     }
